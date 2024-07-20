de58fed1d1d4432aafeba19b570ca99b220683b8
DanielBarletta81 <dbarcelona1481@gmail.com>
2024-07-20 14:56:58 -0400

    add text_utils file, finish assignment

--- main.py
+++ main.py
@@ -4,6 +4,11 @@
 #mood as input (e.g., happy, sad, excited) and return a corresponding custom message.
 
 import mood_module
+from text_utils import make_title, make_encoded
+
 
 mood = input("How are you feeling today? (happy/sad/ambitious/nervous/bored) ")
 print(mood_module.mood_response(mood))
+
+make_title(mood)
+make_encoded(mood)
